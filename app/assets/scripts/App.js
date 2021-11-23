86eabbfdc0f7fa3f976d0fa09b79567e651f148b
Leon <leonndp@gmail.com>
2021-11-24 02:45:02 +0800

    Modules Done

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,12 @@
+import 'normalize.css'
+import '../styles/styles.css';
+
+import MobileMenu from './modules/MobileMenu'
+import Dropdown from './modules/Dropdown'
+
+new MobileMenu();
+new Dropdown()
+
+if (module.hot) {
+    module.hot.accept();
+}
